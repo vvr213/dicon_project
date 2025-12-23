dbb0a76054392488da6f396151da74839d0c192f
vena <vena@venanoMacBook-Pro.local>
2025-12-24 08:35:47 +0900

    fix: configure static files directory

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+#12/24追加
+STATICFILES_DIRS = [
+    BASE_DIR / "static", 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
